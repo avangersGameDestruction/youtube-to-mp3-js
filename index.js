0abe26297eae22b304260efecf2a50f0c237fe87
Dutchy <63201809+avangersKinkyClient@users.noreply.github.com>
2021-11-07 15:36:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 // create a function to download video's and music of youtube show the output inside the console.
 function loadVideo(video) {
+    // npm install youtube-node
     const youTube = require("youtube-node");
+    // npm install fs
     const fs = require("fs");
+    // section for your youtube api key
     const youTubeApiKey = "your api key";
+    // create a new instance of youtube node
     const youTubeApi = new youTube(youTubeApiKey);
+    // npm install path
     const path = require("path");
+    //npm install fluent-ffmpeg
     const ffmpeg = require("fluent-ffmpeg");
+    // youTubeApi.setMaxResults(1); = a function to set the max results
     youTubeApi.setMaxResults(1);
+    // youTubeApi.setKey(youTubeApiKey); = a function to set the api key
     youTubeApi.setKey(youTubeApiKey);
+    // youTubeApi.search(video, 1, function(error, result) = a function to search the video and the max results
     youTubeApi.search(video, 1, function(error, result) {
+        // if there is an error
         if (error) {
+            // console.log(error); = a function to show the error
             console.log(error);
         } else {
             let json = JSON.parse(result);
